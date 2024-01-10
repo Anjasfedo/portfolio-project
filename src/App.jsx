424e0f16b75420b4729bade3f2d5b48eeee3aac0
AnjasFedo <fedoaf98@gmail.com>
2024-01-10 08:36:19 +0700

    Init Repo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import {cn} from "../lib/utils"
+
+function App() {
+
+  return (
+    <div className="relative bg-[#161A30] min-h-screen flex flex-col justify-center items-center mx-auto">
+      
+    </div>
+  )
+}
+
+export default App
